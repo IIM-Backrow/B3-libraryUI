b0afdc50970c73d9d1e141a7dab3cad9cb7e2ff2
Pierric Letard <pierric@radiomail-fr.com>
2025-07-11 10:00:15 +0200

    first card component

---
+++ src/card/Card.stories.ts
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Card from "./Card";
+
+const meta = {
+  title: "Components/Card",
+  component: Card,
+  parameters: {
+    layout: "centered"
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+      options: ["static-rainbow", "dynamic-rainbow"]
+    },
+    size: {
+      control: { type: "select" },
+      options: ["small", "medium", "large"]
+    },
+    onClick: { action: "clicked" }
+  }
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof Card>;
+
+// Define stories
+
+export const StaticRainbow: Story = {
+  args: {
+    variant: "static-rainbow",
+    children: "Static Rainbow Card",
+    size: "medium"
+  }
+};
+export const DynamicRainbow: Story = {
+  args: {
+    variant: "dynamic-rainbow",
+    children: "Dynamic Rainbow Card",
+    size: "medium"
+    }
+};
+export const Small: Story = {
+  args: {
+    children: "Small Card",
+    size: "small"
+  }
+};
+
+export const Large: Story = {
+  args: {
+    children: "Large Card",
+    size: "large"
+  }
+};
