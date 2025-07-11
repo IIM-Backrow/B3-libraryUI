85e68aa7aa572434d0fcfd0cefe9a33977480838
Thomas Candille <114737865+ThomasCandille@users.noreply.github.com>
2025-07-11 10:44:56 +0200

    feat: implement TextInput component with styles, stories, and tests

--- src/textInput/text-input.stories.ts
+++ src/textInput/text-input.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import TextInput from "./TextInput";
+import TextInput from "./text-input";
 
 const meta = {
   title: "Components/TextInput",
