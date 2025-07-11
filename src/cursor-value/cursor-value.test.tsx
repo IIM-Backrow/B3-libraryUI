ab8ff9ec0c99afc5b336e47f7aa68ec26e089879
F1N3X <quartzman92@gmail.com>
2025-07-11 10:42:57 +0200

    applying test logic with new timer type

--- src/cursor-value/cursor-value.test.tsx
+++ src/cursor-value/cursor-value.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import CursorValue from "./cursor-value";
+import { act } from "react";
 
 // Test basique : rendu et changement de valeur
 
@@ -13,13 +14,21 @@ describe("CursorValue Component", () => {
   });
 
   it("calls onChange when the mouse moves left or right", () => {
+    jest.useFakeTimers();
     const handleChange = jest.fn();
     render(
       <CursorValue min={0} max={10} value={5} onChange={handleChange} />
     );
     const container = screen.getByRole("slider");
     fireEvent.mouseMove(container, { clientX: 0 });
+    act(() => {
+      jest.advanceTimersByTime(60);
+    });
     fireEvent.mouseMove(container, { clientX: 999 });
+    act(() => {
+      jest.advanceTimersByTime(60);
+    });
     expect(handleChange).toHaveBeenCalled();
+    jest.useRealTimers();
   });
 });
